f9d4c646380b0991f40a722807e8282ac7be9863
Anssi Kanninen <anssi.kanninen@iki.fi>
2022-10-07 09:36:04 +0300

    simplifying

--- ruuvi_movement.py
+++ ruuvi_movement.py
@@ -35,22 +35,20 @@ print('Listen: ' + str(listen_macs))
 def in_schedule():
     hour_now = datetime.now().hour
     if hour_begin <= hour_end:
-        if hour_now >= hour_begin and hour_now < hour_end:
-            return True
-    else:
-        if hour_now >= hour_begin or hour_now < hour_end:
-            return True
-    return False
+        return hour_begin <= hour_now < hour_end
+    return hour_begin <= hour_now or hour_now < hour_end
 
-def send_alert(msg):
+def send_alert(msg, force_alert = False):
     print(msg)
-    if in_schedule():
+    if force_alert or in_schedule():
+        print("in schedule or forced, alerting")
         response = requests.post(
             webhook,
             headers={'Content-type': 'application/json'},
             data='{"text":\'' + msg + '\'}'
         )
-    
+    else:
+        print("not in schedule")
 
 # Handle timeouts
 def timer_handler(signum, frame):
@@ -61,7 +59,7 @@ def timer_handler(signum, frame):
 #            el
             if timers[idx] != 0:
                 if (datetime.now() - timers[idx]).total_seconds() > tag_timeout:
-                    send_alert('No connection: ' + names[idx])
+                    send_alert('No connection: ' + names[idx], True)
                     timers[idx] = 0
 #                else:
 #                    print('No timeout: ' + names[idx])
@@ -79,7 +77,7 @@ def handle_data(found_data):
     found_name = names[idx]
     move_count = move_counts[idx]
     if timers[idx] == 0:
-        send_alert('Connection resumed: ' + names[idx])
+        send_alert('Connection resumed: ' + names[idx], True)
     timers[idx] = datetime.now()
 #    print (
 #        datetime.now().strftime("%F %H:%M:%S") +
